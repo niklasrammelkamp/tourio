7481c57fcfa646ac25302f10dfc36f430bbd367b
niklasrammelkamp <niklas.rammelkammp@gmail.com>
2023-01-13 14:59:00 +0100

    add backend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import CardList from "@/components/CardList";
+import useSWR from "swr";
 
 export default function Home() {
+  const { data } = useSWR("/api/places");
+  console.log("data", data);
   return <CardList />;
 }
